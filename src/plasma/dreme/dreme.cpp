9a9c5255c78d068f5d44a3209bd6e52ddfe2f401
Jonas Maaskola <jonas@maaskola.de>
2013-11-19 22:54:57 +0100

    Move DREME code into subdirectory of plasma

---
+++ src/plasma/dreme/dreme.cpp
@@ -0,0 +1,92 @@
+#include "dreme.hpp"
+#include "../../hmm/basedefs.hpp"
+#include "../../dreme_config.hpp"
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+namespace Dreme {
+  const char* BinaryNotFoundException::what() const throw() {
+    return "Could not find Dreme binary.";
+  };
+
+  InvalidLengthsException::InvalidLengthsException(size_t s1, size_t s2) :
+    exception(),
+    min_size(s1),
+    max_size(s2)
+  { };
+  const char* InvalidLengthsException::InvalidLengthsException::what() const throw()
+  {
+    return(("min_size (" + boost::lexical_cast<string>(min_size) + ") is not smaller than max_size (" + boost::lexical_cast<string>(max_size) + ").").c_str());
+  };
+
+  list<string> parse_dreme_output(const string &dir) {
+    list<string> motifs;
+
+    string path = dir + "/dreme.txt";
+
+    ifstream ifs(path.c_str());
+
+    string line;
+    while(getline(ifs, line))
+      if(line.substr(0, 6) == "MOTIF ")
+        motifs.push_back(line.substr(6));
+    return(motifs);
+  }
+
+  list<string> run(const string &path1,
+      const string &path2,
+      size_t min_size,
+      size_t max_size,
+      bool revcomp,
+      size_t n_motifs,
+      const string &out_path) {
+    if(path2 == "")
+      cout << "Running DREME for one FASTA file: " << path1 << endl;
+    else
+      cout << "Running DREME for two FASTA files: " << path1 << " " << path2 << endl;
+
+    stringstream str;
+    str << DREME_PATH << " -p " << path1;
+
+    if(path2 != "")
+      str << " -n " << path2;
+
+    if(min_size == max_size) {
+      size_t len = min_size;
+      if(len != 0)
+        str << " -k " << len;
+    } else {
+      if(min_size > max_size)
+        throw InvalidLengthsException(min_size, max_size);
+      str << " -mink " << min_size << " -maxk " << max_size;
+    }
+
+    if(not revcomp)
+      str << " -norc";
+
+    if(n_motifs > 0)
+      str << " -m " << n_motifs;
+
+    string dreme_output_dir;
+    if(out_path == "")
+      dreme_output_dir = "/tmp/dlhmm_dreme_out/";
+    else
+      dreme_output_dir = "/tmp/" + out_path;
+
+    str << " -oc " << dreme_output_dir;
+
+    string command = str.str();
+
+    cout << "Command = " << command << endl;
+
+    int res = system(command.c_str());
+
+    list<string> regexes = parse_dreme_output(dreme_output_dir);
+    for(auto &x: regexes)
+      cout << "Dreme found motif: " << x << endl;
+    return(regexes);
+  }
+}
+
---
+++ src/plasma/dreme/dreme.hpp
@@ -0,0 +1,34 @@
+
+#ifndef DREME_HPP
+
+#include <cstddef>
+#include <list>
+#include <string>
+#include <exception>
+
+namespace Dreme {
+  class BinaryNotFoundException: public std::exception {
+    virtual const char* what() const throw();
+  };
+
+  class InvalidLengthsException: public std::exception {
+    private:
+      size_t min_size, max_size;
+    public:
+      InvalidLengthsException(size_t s1, size_t s2);
+      virtual const char* what() const throw();
+  };
+
+  std::list<std::string> parse_dreme_output(const std::string &dir);
+
+  std::list<std::string> run(const std::string &path1,
+      const std::string &path2="",
+      size_t min_size=0,
+      size_t max_size=0,
+      bool revcomp=false,
+      size_t n_motifs=0,
+      const std::string &out_path="");
+};
+
+#endif
+
