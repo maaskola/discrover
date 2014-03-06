44949580b38fc90162885f3a2ff15f530432d92a
Jonas Maaskola <jonas@maaskola.de>
2014-03-06 21:28:30 +0100

    Print motif number when there are multiple

--- scripts/reldistplot.r
+++ scripts/reldistplot.r
@@ -5,12 +5,21 @@ plot.rel.dist = function(path) {
   y = split(x, x$file)
   for(f in names(y)) {
     z = y[[f]]
-    counts = table(z$centerdist)
-    cnt = as.vector(counts)
-    d = as.numeric(names(counts))
-    print(d)
-    print(cnt)
-    plot(d, cnt, ylim=c(0, max(cnt)), xlab="Distance from center [nt]", ylab="Count", type='h', main=f)
+    m = split(z, z$motifidx)
+    idx = 1
+    for(g in names(m)) {
+      n = m[[g]]
+      counts = table(n$centerdist)
+      cnt = as.vector(counts)
+      d = as.numeric(names(counts))
+      print(d)
+      print(cnt)
+      main = f
+      if(length(m) > 1)
+        main = paste(f, idx)
+      idx = idx + 1
+      plot(d, cnt, ylim=c(0, max(cnt)), xlab="Distance from center [nt]", ylab="Count", type='h', main=main)
+    }
   }
 }
 
