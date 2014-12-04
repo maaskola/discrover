b07d77447bf67d414a3213b8797df88057cfe39b
Jonas Maaskola <jonas@maaskola.de>
2014-12-04 22:10:51 +0100

    Remove two unneeded typedefs

--- src/plasma/count.hpp
+++ src/plasma/count.hpp
@@ -41,9 +41,6 @@ namespace Seeding {
   void add_counts(const std::string &seq, size_t length, hash_map_t &counts, size_t idx, const count_vector_t &default_stats, const Options &options);
   void add_counts(const Set &dataset, size_t len, hash_map_t &counts, size_t idx, const count_vector_t &default_stats, const Options &options);
 
-  using seq_idx_t = uint16_t;
-  using Index = std::map<std::string, std::list<seq_idx_t>>;
-
   hash_map_t get_word_counts(const Collection &collection, size_t length, const Options &options);
   count_vector_t count_motif(const Collection &collection, const std::string &motif, const Options &options);
 
