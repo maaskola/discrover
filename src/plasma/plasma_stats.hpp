b554602337270d7f3964a235262632597666eeb7
Jonas Maaskola <jonas@maaskola.de>
2014-12-04 20:00:05 +0100

    Alternative hashing schemes

--- src/plasma/plasma_stats.hpp
+++ src/plasma/plasma_stats.hpp
@@ -45,10 +45,21 @@ struct hash<seq_type> {
     using std::hash;
     using std::string;
 
-    size_t h = 0;
-    for(auto &x: v)
-      h = h ^ hash<seq_type::value_type>()(x);
-    return h;
+    // slow but compatible with previous, string-based implementation
+    return hash<string>()(decode(v));
+
+    // faster
+    // size_t h = 0;
+    // for(auto &x: v)
+    //   h = h ^ x;
+    // return h;
+
+    // faster, likely the same as above, if the implementation uses identity hashes for integral types
+    // size_t h = 0;
+    // for(auto &x: v)
+    //   h = h ^ hash<seq_type::value_type>()(x);
+    // return h;
+
   }
 };
 }
