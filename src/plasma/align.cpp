c85ebff60b68748fc837315c2167e6141e803c23
Jonas Maaskola <jonas@maaskola.de>
2014-12-07 20:11:27 +0100

    Clang-format

--- src/plasma/align.cpp
+++ src/plasma/align.cpp
@@ -17,16 +17,21 @@ using namespace std;
 
 const char TERMINATOR_SYMBOL = '$';
 
-vector<symbol_t> collapse_collection(const Seeding::Collection &collection, vector<size_t> &pos2seq, vector<size_t> &seq2set, vector<size_t> &set2contrast, bool allow_iupac_wildcards) {
+vector<symbol_t> collapse_collection(const Seeding::Collection &collection,
+                                     vector<size_t> &pos2seq,
+                                     vector<size_t> &seq2set,
+                                     vector<size_t> &set2contrast,
+                                     bool allow_iupac_wildcards) {
   vector<symbol_t> s;
   size_t seq_idx = 0;
   size_t set_idx = 0;
   size_t contrast_idx = 0;
-  for(auto &contrast: collection) {
-    for(auto &dataset: contrast) {
-      for(auto &seq: dataset) {
-        add_sequence(s, seq.sequence + TERMINATOR_SYMBOL, allow_iupac_wildcards);
-        for(size_t i = 0; i < seq.sequence.size() + 1; i++)
+  for (auto &contrast : collection) {
+    for (auto &dataset : contrast) {
+      for (auto &seq : dataset) {
+        add_sequence(s, seq.sequence + TERMINATOR_SYMBOL,
+                     allow_iupac_wildcards);
+        for (size_t i = 0; i < seq.sequence.size() + 1; i++)
           pos2seq.push_back(seq_idx);
         seq2set.push_back(set_idx);
         seq_idx++;
@@ -36,5 +41,5 @@ vector<symbol_t> collapse_collection(const Seeding::Collection &collection, vect
     }
     contrast_idx++;
   }
-  return(s);
+  return s;
 }
--- src/plasma/align.hpp
+++ src/plasma/align.hpp
@@ -38,29 +38,33 @@
 #include "code.hpp"
 #include "data.hpp"
 
-template <typename T> bool binary_and_not_null(T a, T b) {
+template <typename T>
+bool binary_and_not_null(T a, T b) {
   return (a & b) != 0;
 }
 
-template <class data_t, class idx_t=size_t, class lcp_t=size_t, bool shift=false>
+template <class data_t, class idx_t = size_t, class lcp_t = size_t,
+          bool shift = false>
 class Index {
-  public:
-    Index(const data_t &x, Verbosity verbosity) :
-      data(x),
-      // generate suffix array, LCP, and JMP tables
-      sa(gen_suffix_array<shift, idx_t>(begin(data), end(data), verbosity)),
-      lcp(gen_lcp<lcp_t>(begin(data), end(data), sa, verbosity)),
-      jmp(gen_jmp<idx_t>(lcp, verbosity)) { };
+public:
+  Index(const data_t &x, Verbosity verbosity)
+      : data(x),
+        // generate suffix array, LCP, and JMP tables
+        sa(gen_suffix_array<shift, idx_t>(begin(data), end(data), verbosity)),
+        lcp(gen_lcp<lcp_t>(begin(data), end(data), sa, verbosity)),
+        jmp(gen_jmp<idx_t>(lcp, verbosity)){};
 
-    template<class Cmp=std::equal_to<typename data_t::value_type>>
-    std::vector<idx_t> find_matches(const data_t &query, Cmp cmp=Cmp()) const {
-      return(match(begin(query), end(query), begin(data), end(data), sa, lcp, jmp, cmp));
-    };
-  private:
-    data_t data;               // the original data
-    std::vector<idx_t> sa;     // suffix array
-    std::vector<lcp_t> lcp;    // LCP table
-    std::vector<idx_t> jmp;    // JMP table
+  template <class Cmp = std::equal_to<typename data_t::value_type>>
+  std::vector<idx_t> find_matches(const data_t &query, Cmp cmp = Cmp()) const {
+    return match(begin(query), end(query), begin(data), end(data), sa, lcp, jmp,
+                 cmp);
+  };
+
+private:
+  data_t data;             // the original data
+  std::vector<idx_t> sa;   // suffix array
+  std::vector<lcp_t> lcp;  // LCP table
+  std::vector<idx_t> jmp;  // JMP table
 };
 
 seq_type collapse_collection(const Seeding::Collection &collection,
@@ -69,96 +73,99 @@ seq_type collapse_collection(const Seeding::Collection &collection,
                              std::vector<size_t> &set2contrast,
                              bool allow_iupac_wildcards);
 
-template <class idx_t=size_t, class lcp_t=size_t, class base_t=seq_type, class index_t=Index<base_t, idx_t, lcp_t, true>>
+template <class idx_t = size_t, class lcp_t = size_t, class base_t = seq_type,
+          class index_t = Index<base_t, idx_t, lcp_t, true>>
 class NucleotideIndex {
-  public:
-    using base_type = base_t;
+public:
+  using base_type = base_t;
 
-    NucleotideIndex(const NucleotideIndex &i) :
-      paths(i.paths),
-      pos2seq(i.pos2seq),
-      seq2set(i.seq2set),
-      set2contrast(i.set2contrast),
-      index(i.index) { };
+  NucleotideIndex(const NucleotideIndex &i)
+      : paths(i.paths),
+        pos2seq(i.pos2seq),
+        seq2set(i.seq2set),
+        set2contrast(i.set2contrast),
+        index(i.index){};
 
-    NucleotideIndex(Verbosity verbosity=Verbosity::info) :
-      paths(),
-      pos2seq(),
-      seq2set(),
-      set2contrast(),
-      index({}, verbosity) { };
+  NucleotideIndex(Verbosity verbosity = Verbosity::info)
+      : paths(), pos2seq(), seq2set(), set2contrast(), index({}, verbosity){};
 
-    NucleotideIndex(const Seeding::Collection &collection, bool allow_iupac_wildcards, Verbosity verbosity) :
-      paths(),
-      pos2seq(),
-      seq2set(),
-      set2contrast(),
-      index(collapse_collection(collection, pos2seq, seq2set, set2contrast, allow_iupac_wildcards), verbosity) {
-        for(auto &contrast: collection)
-          for(auto &dataset: contrast)
-            paths.push_back(dataset.path);
-      };
+  NucleotideIndex(const Seeding::Collection &collection,
+                  bool allow_iupac_wildcards, Verbosity verbosity)
+      : paths(),
+        pos2seq(),
+        seq2set(),
+        set2contrast(),
+        index(collapse_collection(collection, pos2seq, seq2set, set2contrast,
+                                  allow_iupac_wildcards),
+              verbosity) {
+    for (auto &contrast : collection)
+      for (auto &dataset : contrast)
+        paths.push_back(dataset.path);
+  };
 
-    std::vector<size_t> word_hits_by_file(const base_type &query, bool revcomp=false) const {
-      std::vector<size_t> counts(paths.size(),0);
-      for(auto &p: index.find_matches(query, binary_and_not_null<symbol_t>)) {
-        size_t seqIdx = pos2seq[p];
-        size_t fileIdx = seq2set[seqIdx];
-        counts[fileIdx]++;
-      }
-      if(revcomp) {
-        auto rc = iupac_reverse_complement(query);
-        if(rc != query)
-          for(auto &p: index.find_matches(rc, binary_and_not_null<symbol_t>)) {
-            size_t seqIdx = pos2seq[p];
-            size_t fileIdx = seq2set[seqIdx];
-            counts[fileIdx]++;
-          }
-      }
-      return(counts);
-    };
+  std::vector<size_t> word_hits_by_file(const base_type &query,
+                                        bool revcomp = false) const {
+    std::vector<size_t> counts(paths.size(), 0);
+    for (auto &p : index.find_matches(query, binary_and_not_null<symbol_t>)) {
+      size_t seqIdx = pos2seq[p];
+      size_t fileIdx = seq2set[seqIdx];
+      counts[fileIdx]++;
+    }
+    if (revcomp) {
+      auto rc = iupac_reverse_complement(query);
+      if (rc != query)
+        for (auto &p : index.find_matches(rc, binary_and_not_null<symbol_t>)) {
+          size_t seqIdx = pos2seq[p];
+          size_t fileIdx = seq2set[seqIdx];
+          counts[fileIdx]++;
+        }
+    }
+    return counts;
+  };
 
-    std::vector<size_t> seq_hits_by_file2(const base_type &query, bool revcomp=false) const {
-      std::unordered_set<size_t> seqs;
-      for(auto &p: index.find_matches(query, binary_and_not_null<symbol_t>))
-        seqs.insert(pos2seq[p]);
-      if(revcomp) {
-        auto rc = iupac_reverse_complement(query);
-        if(rc != query)
-          for(auto &p: index.find_matches(rc, binary_and_not_null<symbol_t>))
-            seqs.insert(pos2seq[p]);
-      }
+  std::vector<size_t> seq_hits_by_file2(const base_type &query,
+                                        bool revcomp = false) const {
+    std::unordered_set<size_t> seqs;
+    for (auto &p : index.find_matches(query, binary_and_not_null<symbol_t>))
+      seqs.insert(pos2seq[p]);
+    if (revcomp) {
+      auto rc = iupac_reverse_complement(query);
+      if (rc != query)
+        for (auto &p : index.find_matches(rc, binary_and_not_null<symbol_t>))
+          seqs.insert(pos2seq[p]);
+    }
 
-      std::vector<size_t> counts(paths.size(),0);
-      for(auto &s: seqs)
-        counts[seq2set[s]]++;
-      return(counts);
-    };
+    std::vector<size_t> counts(paths.size(), 0);
+    for (auto &s : seqs)
+      counts[seq2set[s]]++;
+    return counts;
+  };
 
-    std::vector<size_t> seq_hits_by_file(const base_type &query, bool revcomp=false) const {
-      std::vector<size_t> seqs;
-      for(auto &p: index.find_matches(query, binary_and_not_null<symbol_t>))
-        seqs.push_back(pos2seq[p]);
-      if(revcomp) {
-        auto rc = iupac_reverse_complement(query);
-        if(rc != query)
-          for(auto &p: index.find_matches(rc, binary_and_not_null<symbol_t>))
-            seqs.push_back(pos2seq[p]);
-      }
+  std::vector<size_t> seq_hits_by_file(const base_type &query,
+                                       bool revcomp = false) const {
+    std::vector<size_t> seqs;
+    for (auto &p : index.find_matches(query, binary_and_not_null<symbol_t>))
+      seqs.push_back(pos2seq[p]);
+    if (revcomp) {
+      auto rc = iupac_reverse_complement(query);
+      if (rc != query)
+        for (auto &p : index.find_matches(rc, binary_and_not_null<symbol_t>))
+          seqs.push_back(pos2seq[p]);
+    }
 
-      std::sort(begin(seqs), end(seqs));
-      seqs.resize(std::unique(begin(seqs), end(seqs)) - begin(seqs));
+    std::sort(begin(seqs), end(seqs));
+    seqs.resize(std::unique(begin(seqs), end(seqs)) - begin(seqs));
 
-      std::vector<size_t> counts(paths.size(),0);
-      for(auto &s: seqs)
-        counts[seq2set[s]]++;
-      return(counts);
-    };
+    std::vector<size_t> counts(paths.size(), 0);
+    for (auto &s : seqs)
+      counts[seq2set[s]]++;
+    return counts;
+  };
 
-  private:
-    std::vector<std::string> paths;
-    std::vector<size_t> pos2seq, seq2set, set2contrast;
-    index_t index;
+private:
+  std::vector<std::string> paths;
+  std::vector<size_t> pos2seq, seq2set, set2contrast;
+  index_t index;
 };
 
 #endif
