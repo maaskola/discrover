a4db996b53c2e094acdbb5cb86620854dd46039b
Jonas Maaskola <jonas@maaskola.de>
2015-01-02 03:30:35 +0100

    Draw letter T

--- src/logo/logo.cpp
+++ src/logo/logo.cpp
@@ -48,8 +48,19 @@ coords_t letter_a = {
   {3.6, 4}
 };
 
+coords_t letter_t = {
+  {10, 10},
+  {10, 9},
+  {6, 9},
+  {6, 0},
+  {4, 0},
+  {4, 9},
+  {0, 9},
+  {0, 10}
+};
+
 void draw_letter(cairo_t *cr, const coords_t &coords, double x, double y, double width, double height, rgb_t color) {
-  cairo_move_to(cr, x, y);
+  cairo_move_to(cr, x + coords[0].x / 10 * width, y - coords[0].y / 10 * height);
   for(auto coord: coords)
     cairo_line_to(cr, x + coord.x / 10 * width, y - coord.y / 10 * height);
   cairo_set_line_width (cr, 1.0);
@@ -74,6 +85,9 @@ bool draw_logo(const string &path) {
   cairo_show_text (cr, "Hello, world");
 
   draw_letter(cr, letter_a, 10, 50, 30, 30, default_colors.a);
+  draw_letter(cr, letter_t, 40, 50, 30, 30, default_colors.c);
+  draw_letter(cr, letter_t, 40, 80, 30, 30, default_colors.g);
+  draw_letter(cr, letter_a, 40, 20, 30, 30, default_colors.t);
 
   cairo_destroy (cr);
   cairo_surface_write_to_png (surface, out_path.c_str());
