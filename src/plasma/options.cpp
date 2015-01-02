cd5c64cf0575de31ee575ae945d4f0be5fa52b8d
Jonas Maaskola <jonas@maaskola.de>
2015-01-02 22:15:27 +0100

    Clang-format

--- src/plasma/options.cpp
+++ src/plasma/options.cpp
@@ -18,139 +18,136 @@
 
 using namespace std;
 
-
 namespace Seeding {
 
-  Options::Options() :
-    paths(),
-    motif_specifications({}),
-    objectives(),
-    algorithm(Algorithm::Plasma),
-    plasma(Plasma()),
-    n_threads(1),
-    revcomp(false),
-    strict(false),
-    pseudo_count(1),
-    weighting(false),
-    n_seq(0),
-    word_stats(false),
-    measure_runtime(false),
-    occurrence_filter(OccurrenceFilter::RemoveSequences),
-    only_best(false),
-    verbosity(Verbosity::info),
-    dump_viterbi(false),
-    no_enrichment_filter(false),
-    fixed_motif_space_mode(false),
-    allow_iupac_wildcards(false),
-    label("")
-    { };
+Options::Options()
+    : paths(),
+      motif_specifications({}),
+      objectives(),
+      algorithm(Algorithm::Plasma),
+      plasma(Plasma()),
+      n_threads(1),
+      revcomp(false),
+      strict(false),
+      pseudo_count(1),
+      weighting(false),
+      n_seq(0),
+      word_stats(false),
+      measure_runtime(false),
+      occurrence_filter(OccurrenceFilter::RemoveSequences),
+      only_best(false),
+      verbosity(Verbosity::info),
+      dump_viterbi(false),
+      no_enrichment_filter(false),
+      fixed_motif_space_mode(false),
+      allow_iupac_wildcards(false),
+      label(""){};
 
-  Options::Plasma::Plasma() :
-    max_candidates(100),
-    degeneracies(),
-    rel_degeneracy(1),
-    per_degeneracy(false)
-  { };
+Options::Plasma::Plasma()
+    : max_candidates(100),
+      degeneracies(),
+      rel_degeneracy(1),
+      per_degeneracy(false){};
 
-  Options::MCMC::MCMC() :
-    max_iter(1000),
-    temperature(1e-3),
-    n_parallel(6),
-    random_salt(generate_rng_seed())
-  { };
+Options::MCMC::MCMC()
+    : max_iter(1000),
+      temperature(1e-3),
+      n_parallel(6),
+      random_salt(generate_rng_seed()){};
 
-  istream &operator>>(istream &in, OccurrenceFilter &filter) {
-    string token;
-    in >> token;
-    if(token == "remove")
-      filter = OccurrenceFilter::RemoveSequences;
-    else if(token == "mask")
-      filter = OccurrenceFilter::MaskOccurrences;
-    else {
-      cout << "Couldn't parse occurrence filter type '" << token << "'." << endl;
-      exit(-1);
-    }
-    return(in);
+istream &operator>>(istream &in, OccurrenceFilter &filter) {
+  string token;
+  in >> token;
+  if (token == "remove")
+    filter = OccurrenceFilter::RemoveSequences;
+  else if (token == "mask")
+    filter = OccurrenceFilter::MaskOccurrences;
+  else {
+    cout << "Couldn't parse occurrence filter type '" << token << "'." << endl;
+    exit(-1);
   }
+  return in;
+}
 
-  ostream &operator<<(ostream &os, const OccurrenceFilter &filter) {
-    switch(filter) {
-      case OccurrenceFilter::RemoveSequences:
-        os << "remove";
-        break;
-      case OccurrenceFilter::MaskOccurrences:
-        os << "mask";
-        break;
-    }
-    return(os);
+ostream &operator<<(ostream &os, const OccurrenceFilter &filter) {
+  switch (filter) {
+    case OccurrenceFilter::RemoveSequences:
+      os << "remove";
+      break;
+    case OccurrenceFilter::MaskOccurrences:
+      os << "mask";
+      break;
   }
+  return os;
+}
 
-  Algorithm parse_algorithm(const string &token_) {
-    string token(token_);
-    boost::algorithm::to_lower(token);
-    if(token == "plasma")
-      return(Algorithm::Plasma);
-    else if(token == "dreme")
-      return(Algorithm::ExternalDREME);
-    else if(token == "mcmc")
-      return(Algorithm::MCMC);
-    else if(token == "all")
-      return(Algorithm::Plasma | Algorithm::ExternalDREME | Algorithm::MCMC);
-    else {
-      cout << "Seeding algorithm '" << token_ << "' unknown." << endl
-        << "Please use one of 'plasma', 'dreme', 'mcmc', or 'all'." << endl
-        << "It is also possible to use multiple algorithms by separating them by comma." << endl;
-      exit(-1);
-    }
-  }
-
-  istream &operator>>(istream &in, Algorithm &algorithm) {
-    string algorithms;
-    in >> algorithms;
-    bool first = true;
-    size_t pos;
-    do {
-      pos = algorithms.find(",");
-      Algorithm algo = parse_algorithm(algorithms.substr(0, pos));
-      if(first) {
-        first = false;
-        algorithm = algo;
-      } else
-        algorithm = algorithm | algo;
-      algorithms = algorithms.substr(pos+1);
-    } while(pos != string::npos);
-    Algorithm algo = parse_algorithm(algorithms);
-    if(first)
-      algorithm = algo;
-    else
-      algorithm = algorithm | algo;
-    return(in);
-  }
-  ostream &operator<<(ostream &os, const Algorithm &algorithm) {
-    bool first = true;
-    if((algorithm & Algorithm::Plasma) == Algorithm::Plasma) {
-      os << "plasma";
-      first = false;
-    }
-    if((algorithm & Algorithm::ExternalDREME) == Algorithm::ExternalDREME) {
-      os << (first ? "" : ",") << "dreme";
-      first = false;
-    }
-    if((algorithm & Algorithm::MCMC) == Algorithm::MCMC)
-      os << (first ? "" : ",") << "mcmc";
-    return(os);
-  }
-
-  ostream &operator<<(ostream &out, const Objective &objective) {
-    out << Specification::to_string(objective);
-    return(out);
-  }
-
-  Objective objective_for_motif(const Objectives &objectives, const Specification::Motif &motif) {
-    for(auto objective: objectives)
-      if(objective.motif_name == motif.name)
-        return(objective);
-    throw("bla");
+Algorithm parse_algorithm(const string &token_) {
+  string token(token_);
+  boost::algorithm::to_lower(token);
+  if (token == "plasma")
+    return Algorithm::Plasma;
+  else if (token == "dreme")
+    return Algorithm::ExternalDREME;
+  else if (token == "mcmc")
+    return Algorithm::MCMC;
+  else if (token == "all")
+    return Algorithm::Plasma | Algorithm::ExternalDREME | Algorithm::MCMC;
+  else {
+    cout << "Seeding algorithm '" << token_ << "' unknown." << endl
+         << "Please use one of 'plasma', 'dreme', 'mcmc', or 'all'." << endl
+         << "It is also possible to use multiple algorithms by separating them "
+            "by comma." << endl;
+    exit(-1);
   }
 }
 
+istream &operator>>(istream &in, Algorithm &algorithm) {
+  string algorithms;
+  in >> algorithms;
+  bool first = true;
+  size_t pos;
+  do {
+    pos = algorithms.find(",");
+    Algorithm algo = parse_algorithm(algorithms.substr(0, pos));
+    if (first) {
+      first = false;
+      algorithm = algo;
+    } else
+      algorithm = algorithm | algo;
+    algorithms = algorithms.substr(pos + 1);
+  } while (pos != string::npos);
+  Algorithm algo = parse_algorithm(algorithms);
+  if (first)
+    algorithm = algo;
+  else
+    algorithm = algorithm | algo;
+  return in;
+}
+ostream &operator<<(ostream &os, const Algorithm &algorithm) {
+  bool first = true;
+  if ((algorithm & Algorithm::Plasma) == Algorithm::Plasma) {
+    os << "plasma";
+    first = false;
+  }
+  if ((algorithm & Algorithm::ExternalDREME) == Algorithm::ExternalDREME) {
+    os << (first ? "" : ",") << "dreme";
+    first = false;
+  }
+  if ((algorithm & Algorithm::MCMC) == Algorithm::MCMC)
+    os << (first ? "" : ",") << "mcmc";
+  return os;
+}
+
+ostream &operator<<(ostream &out, const Objective &objective) {
+  out << Specification::to_string(objective);
+  return out;
+}
+
+Objective objective_for_motif(const Objectives &objectives,
+                              const Specification::Motif &motif) {
+  for (auto objective : objectives)
+    if (objective.motif_name == motif.name)
+      return objective;
+  throw("bla");
+}
+}
