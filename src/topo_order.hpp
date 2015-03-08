c1935f2e3a062a1712bcd4b6f50be770a77ab48b
Jonas Maaskola <jonas@maaskola.de>
2015-03-08 17:15:24 +0100

    Move topological ordering code into separate source file

---
+++ src/topo_order.hpp
@@ -0,0 +1,34 @@
+/* =====================================================================================
+ * Copyright (c) 2015, Jonas Maaskola
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * =====================================================================================
+ *
+ *       Filename:  topo_order.hpp
+ *
+ *    Description:  Compute topological ordering
+ *
+ *        Created:  Sun Mar 8 16:11:23 2015 +0100
+ *
+ *         Author:  Jonas Maaskola <jonas@maaskola.de>
+ *
+ * =====================================================================================
+ */
+
+#include <vector>
+#include "matrix.hpp"
+
+std::vector<size_t> topological_order(const matrix_t &transition,
+                                      const std::vector<size_t> &states);
